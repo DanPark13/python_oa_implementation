83da99daec021c8163e6376147a4d522ebd6688f
Your Name <you@example.com>
2021-04-28 00:27:16 -0400

    Completed Rough Implementation of OA with several TODOS
    - Clean the Code
    - Make Implementation Work with Second Example

--- oa_implementation.py
+++ oa_implementation.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 # Test Case #1 from Instructions
 reference_trajectory_dict = {
     "C":1,
@@ -7,18 +9,20 @@ reference_trajectory_dict = {
     "E":3
 }
 
+print(f"Original Reference Trajectory: {reference_trajectory_dict}\n")
+
 '''
 Original Algorithm Implementation
 '''
 def oa_sort(array):
     # For every number from first number
-    for i in range(len(array)):
+    for compare in range(len(array)):
         # Compare the number above to each subsequent number after that number
-        for j in range(i + 1, len(array)):
-            # If the "i" number is greater than the "j" number
-            if(array[i] > array[j]):
+        for compared_to in range(compare + 1, len(array)):
+            # If the compare number is greater than the number that is being compared to
+            if(array[compare] > array[compared_to]):
                 # Switch places
-                array[i], array[j] = array[j], array[i]
+                array[compare], array[compared_to] = array[compared_to], array[compare]
     return array
 
 # Sort the trajectory value
@@ -28,7 +32,7 @@ sorted_note_dict = {}
 ''' 
 Sort the reference trajectory 
 '''
-def sort_reference_notes(array,dict):
+def sort_reference_notes(array,dict,empty_dict):
     # For each trajectory value
     for i in array:
         # For each note
@@ -36,9 +40,42 @@ def sort_reference_notes(array,dict):
             # If the trajectory value of the note is equal to the trajectory value found in the sorted array
             if dict[k] == i:
                 # Add the note along with its trajectory value to the new dictionary
-                sorted_note_dict[k] = dict[k]
+                empty_dict[k] = dict[k]
     # Return the new dictionary when done
-    return sorted_note_dict
+    return empty_dict
 
 # Print the sorted notes along with their corresponding trajectories
-print(sort_reference_notes(sorted_trajs, reference_trajectory_dict))
+sorted_notes = sort_reference_notes(sorted_trajs, reference_trajectory_dict,sorted_note_dict)
+print(sorted_notes)
+
+variation_trajectory = [0.5,-1,2,3,4]
+
+index_added_number = 0
+variation_number_list = []
+
+# Find the values of trajectory values 
+for num_index, num in enumerate(variation_trajectory):
+    for value_index, value in enumerate(list(sorted_notes.values())[::-1]):
+        if (num <= value):
+            index_added_number = (list(sorted_notes.values())[::-1])[-1]
+        else:
+            # Catch out of bound indeces because index-1 doesn't return error
+            if (list(sorted_notes.values())[::-1])[value_index-1] == (list(sorted_notes.values())[::-1])[-1]:
+                index_added_number = (list(sorted_notes.values())[::-1])[0]
+                break
+            index_added_number = (list(sorted_notes.values())[::-1])[value_index-1]
+            break
+    variation_number_list.append(index_added_number)
+
+variation_notes = []
+
+# For each trajectory value
+for i in variation_number_list:
+    # For each note
+    for k in sorted_notes.keys():
+        # If the trajectory value of the note is equal to the trajectory value found in the sorted array
+        if sorted_notes[k] == i:
+            variation_notes.append(k)
+            break
+
+print(variation_notes)
